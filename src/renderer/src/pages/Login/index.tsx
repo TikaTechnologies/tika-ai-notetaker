76a9f92509683415d5c457ed6d9975dde573408f
Dino Delic <dino.delic.dev@gmail.com>
2025-04-03 10:51:42 +0200

    Added input fields on login, auto focus on render

--- src/renderer/src/pages/Login/index.tsx
+++ src/renderer/src/pages/Login/index.tsx
@@ -1,24 +1,47 @@
-import Versions from '../../components/Versions'
+// import Versions from '../../components/Versions'
+import { useEffect, useRef } from 'react'
 import electronLogo from '../../assets/electron.svg'
 import { useNavigate } from 'react-router'
 
 const LoginPage: React.FC = (): JSX.Element => {
   const navigate = useNavigate()
+  const emailInputRef = useRef<HTMLInputElement>(null)
 
   const navToHomePage = () => {
-    navigate("/home")
+    navigate('/home')
   }
+
+  useEffect(() => {
+    if (emailInputRef.current) {
+      emailInputRef.current.focus()
+    }
+  }, [])
 
   return (
     <>
       <img alt="logo" className="logo" src={electronLogo} />
-      <div className="text">Login Page</div>
+      <div className="text">Login</div>
+      <div className="flex flex-col">
+        <label htmlFor="user_email">Email</label>
+        <input
+          type="email"
+          className="border border-white focus:border-red-500"
+          id="user_email"
+          ref={emailInputRef}
+        />
+        <label htmlFor="user_email">Password</label>
+        <input
+          type="password"
+          className="border border-white  focus:border-red-500"
+          id="user_password"
+        />
+      </div>
       <div className="actions">
         <div className="action">
           <span onClick={navToHomePage}>Return</span>
         </div>
       </div>
-      <Versions></Versions>
+      {/* <Versions></Versions> */}
     </>
   )
 }
